b842c23ef0c8346e1d41f6debc553e4e48167007
David Haberthür <email@davidhaberthuer.ch>
2026-03-12 10:32:04 +0100

    Install dependencies

--- build/pagenum.sh
+++ build/pagenum.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Install dependencies, like in ci/install-spellcheck.sh
+# Set options for extra caution & debugging
+set -o errexit \
+    -o pipefail
+# --allow-releaseinfo-change for error on appveyor like "Repository ... changed its 'Label' value"
+sudo apt-get update --yes --allow-releaseinfo-change
+sudo apt-get install --yes pdftk-java ghostscript enscript
+# Commands for linenumbering a PDF
 input="$1"
 output="${1%.pdf}-num.pdf"
 pagenum=$(pdftk "$input" dump_data | grep "NumberOfPages" | cut -d":" -f2)
